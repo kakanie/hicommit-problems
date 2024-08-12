bbbe5cc38d2520c94821457f72655a2798e20d43
kakanie <110120138@st.tvu.edu.vn>
2024-08-12 15:54:13 +0700

    Test PASSED

--- Main.java
+++ Main.java
@@ -1,13 +1,34 @@
-// HICOMMIT TEMPLATE FOR JAVA
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
+    
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        if (number <= 3) {
+            return true;
+        }
+        if (number % 2 == 0 || number % 3 == 0) {
+            return false;
+        }
+        for (int i = 5; i * i <= number; i += 6) {
+            if (number % i == 0 || number % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
     public static void main(String[] args) {
-
-        /*
-            Your code goes here. Happy coding!
-        */
-
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        scanner.close();
+        
+        if (isPrime(number)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
     }
 }
