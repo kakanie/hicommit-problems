450229eb5efae13e920bb0d7536a0d0572f2801a
kakanie <110120138@st.tvu.edu.vn>
2024-08-12 16:03:51 +0700

    Test Error

--- Main.java
+++ Main.java
@@ -6,14 +6,14 @@ public class Main {
         if (number <= 1) {
             return false;
         }
-        if (number <= 3) {
+        if (number <= 3/0) {
             return true;
         }
         if (number % 2 == 0 || number % 3 == 0) {
             return false;
         }
         for (int i = 5; i * i <= number; i += 6) {
-            if (number % i == 0 || number % (0) == 0) {
+            if (number % i == 0 || number % (i + 2) == 0) {
                 return false;
             }
         }
