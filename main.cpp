2b7ee351e87a3d6055b2006368a619662ce08c22
kakanie <110120138@st.tvu.edu.vn>
2024-08-23 17:42:27 +0700

    Submit code at 18:41:17 23/8/2024 by HiCommit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,3 @@
-/*
-    HiCommit - Problem 1
-    Available at: https://hicommit.kakanie.com/problem/1
-*/
-
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -10,10 +5,6 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
-    /*
-        Your code goes here. Happy coding!
-    */
-
+    
     return 0;
 }
